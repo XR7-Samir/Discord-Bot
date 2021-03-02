95224e019ae9cb6d633051140942cb7dc1f30484
Glitch (alpine-ionized-handstand) <none>
2021-03-02 17:23:14 +0000

    🚲🐜 Checkpoint ./config.js:24804239/80

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `--`; //your prefix for bot
-exports.Token = `NzkzOTIxMjU1ODg3NzMyNzY2.X-zSyQ.o48q_Yj9v1uFRPa9sCS79Q0SV6U`; //your token
+exports.Token = ``;
 exports.Color = `Blue`; //color   of embed
